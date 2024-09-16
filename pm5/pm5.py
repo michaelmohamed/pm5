b25073834341601445d05fc1a36612ee91c2eb28
Michael Mohamed <michael@foundationstack.com>
2024-09-16 17:34:38 -0400

    update

--- pm5/pm5.py
+++ pm5/pm5.py
@@ -20,7 +20,6 @@ lock = Lock()  # Lock to handle thread synchronization
 
 shutdown = False  # Global flag to indicate if the system is shutting down
 
-
 # Global list to keep track of running processes and a dictionary to map process IDs to service names
 processes = []
 process_service_map = {}
@@ -34,18 +33,15 @@ def read_config(file_path):
 
 # Function to start a service instance
 def start_service(service, instance_id):
-    # Construct the command to start the service
     command = [service["interpreter"]] + service.get("interpreter_args", [])
     if service["script"]:
         command.append(service["script"])
     command += service.get("args", [])
 
     env = os.environ.copy()  # Copy current environment variables
-
     env.update(
         {k: str(v) for k, v in service.get("env", {}).items()}
     )  # Update with service-specific environment variables
-
     cwd = service.get(
         "cwd", os.getcwd()
     )  # Use specified cwd or current working directory if not set
@@ -56,12 +52,10 @@ def start_service(service, instance_id):
 
     with lock:  # Ensure thread-safe access
         processes.append(process)  # Add process to the list
-
         process_service_map[process.pid] = service[
             "name"
         ]  # Map process ID to service name
-
-        update_lock_file()  # Update the lock file with running process IDs
+        update_lock_file()  # Update the lock file with running process IDs and service names
 
     logger.info(
         f"Starting instance {instance_id} of service '{service['name']}' with command: {' '.join(command)} (PID: {process.pid}) in directory: {cwd}"
@@ -77,7 +71,6 @@ def monitor_service(service, process, instance_id):
     max_restarts = service.get(
         "max_restarts", 0
     )  # Get maximum number of restarts allowed
-
     restarts = 0  # Initialize restart count
 
     while restarts <= max_restarts and not shutdown:
@@ -86,7 +79,6 @@ def monitor_service(service, process, instance_id):
         with lock:
             if process in processes:
                 processes.remove(process)  # Remove process from the list
-
                 update_lock_file()  # Update the lock file
 
         if shutdown:
@@ -101,9 +93,7 @@ def monitor_service(service, process, instance_id):
             logger.info(
                 f"Restarting instance {instance_id} of service '{service['name']}' (Restart {restarts + 1})"
             )
-
             process = start_service(service, instance_id)  # Restart the service
-
             restarts += 1  # Increment restart count
 
         elif restarts == max_restarts:
@@ -111,7 +101,6 @@ def monitor_service(service, process, instance_id):
                 logger.warning(
                     f"Instance {instance_id} of service '{service['name']}' has exceeded the maximum number of restarts ({max_restarts}). Stopping all services."
                 )
-
                 handle_exit(signal.SIGTERM, None)  # Exit if max restarts exceeded
 
         else:
@@ -119,7 +108,6 @@ def monitor_service(service, process, instance_id):
                 logger.error(
                     f"Instance {instance_id} of service '{service['name']}' has exited with an error and will not be restarted"
                 )
-
             break
 
 
@@ -127,9 +115,8 @@ def monitor_service(service, process, instance_id):
 def cleanup_processes():
     global shutdown
 
-    if shutdown is True:
+    if shutdown:
         logger.warning("Shutting down all services. Please hold...")
-
         return
 
     shutdown = True
@@ -137,38 +124,31 @@ def cleanup_processes():
     with lock:
         for process in processes:
             service_name = process_service_map.get(process.pid, "Unknown service")
-
             try:
                 logger.info(
                     f"Sending SIGTERM to process group {os.getpgid(process.pid)} of service '{service_name}'"
                 )
-
                 os.killpg(
                     os.getpgid(process.pid), signal.SIGTERM
                 )  # Send SIGTERM to process group
-
             except Exception as e:
                 logger.info(
                     f"Error sending SIGTERM to process group {os.getpgid(process.pid)} of service '{service_name}': {e}"
                 )
 
         logger.info("Cleaning up services...")
-
         time.sleep(1)  # Give some time for processes to terminate gracefully
 
         for process in processes:
             service_name = process_service_map.get(process.pid, "Unknown service")
-
             if process.poll() is None:  # Check if process is still running
                 try:
                     logger.info(
                         f"Sending SIGKILL to process group {os.getpgid(process.pid)} of service '{service_name}'"
                     )
-
                     os.killpg(
                         os.getpgid(process.pid), signal.SIGKILL
                     )  # Send SIGKILL to process group
-
                 except Exception as e:
                     logger.info(
                         f"Error sending SIGKILL to process group {os.getpgid(process.pid)} of service '{service_name}': {e}"
@@ -177,10 +157,8 @@ def cleanup_processes():
         for process in processes:
             try:
                 process.wait(timeout=5)  # Wait for process to terminate
-
             except subprocess.TimeoutExpired:
                 service_name = process_service_map.get(process.pid, "Unknown service")
-
                 logger.warning(
                     f"Process {process.pid} of service '{service_name}' did not terminate in time"
                 )
@@ -194,11 +172,10 @@ def cleanup_processes():
 def handle_exit(signum, frame):
     global shutdown
 
-    if shutdown is True:
+    if shutdown:
         return
 
     logger.info("Terminating services...")
-
     cleanup_processes()
 
     with lock:
@@ -208,7 +185,6 @@ def handle_exit(signum, frame):
                     logger.warning(
                         f"Process {process.pid} is still running, forcing exit..."
                     )
-
                     os.killpg(
                         os.getpgid(process.pid), signal.SIGKILL
                     )  # Force kill remaining processes
@@ -216,25 +192,21 @@ def handle_exit(signum, frame):
     os._exit(1)  # Force exit with error
 
 
-# Function to read the lock file containing process IDs
+# Function to read the lock file containing process-service mappings
 def read_lock_file():
     if os.path.exists(LOCK_FILE):
         with open(LOCK_FILE, "r") as file:
             try:
                 return json.load(file)
-
             except json.JSONDecodeError:
-                return []
+                return {}
+    return {}
 
-    return []
 
-
-# Function to update the lock file with current process IDs
+# Function to update the lock file with current process-service mapping
 def update_lock_file():
-    pids = [process.pid for process in processes]
-
     with open(LOCK_FILE, "w") as file:
-        json.dump(pids, file)
+        json.dump(process_service_map, file)
 
 
 # Function to clear the lock file
@@ -245,16 +217,16 @@ def clear_lock_file():
 
 # Function to terminate existing processes from the lock file
 def terminate_existing_processes():
-    pids = read_lock_file()
-    active_pids = []
+    service_map = read_lock_file()
+    active_service_map = {}
 
-    for pid in pids:
+    for pid, service_name in service_map.items():
         try:
-            # Verify if the process group is still running
-            os.killpg(pid, 0)
+            pid = int(pid)
+            os.killpg(pid, 0)  # Verify if the process group is still running
             logger.info(f"Terminating existing process group with pid: {pid}")
             os.killpg(pid, signal.SIGTERM)
-            active_pids.append(pid)
+            active_service_map[pid] = service_name
         except ProcessLookupError:
             logger.warning(f"No process group found with pid: {pid}")
         except PermissionError:
@@ -262,61 +234,53 @@ def terminate_existing_processes():
                 f"Permission denied to terminate process group with pid: {pid}"
             )
 
-    # Update the lock file with only the active pids, if any, or clear it
-    if active_pids:
+    # Update the lock file with only the active services, if any, or clear it
+    if active_service_map:
         with open(LOCK_FILE, "w") as file:
-            json.dump(active_pids, file)
+            json.dump(active_service_map, file)
     else:
         clear_lock_file()
 
 
 # Function to show the status of the processes
 def show_status():
-    pids = read_lock_file()
-    if not pids:
+    service_map = read_lock_file()
+    if not service_map:
         logger.info("No processes are currently running.")
         return
 
     logger.info("Current status of managed processes:")
-    for pid in pids:
+    for pid, service_name in service_map.items():
         try:
+            pid = int(pid)
             os.kill(pid, 0)  # Check if the process is still running
             status = "Running"
         except ProcessLookupError:
             status = "Not Running"
-        service_name = process_service_map.get(pid, "Unknown service")
         logger.info(f"Service: {service_name}, PID: {pid}, Status: {status}")
 
 
 # Main function to start the process manager
 def main(**kwargs):
-    # Print the PID of the current Python process
     logger.debug(f"The process manager process id is: {os.getpid()}")
 
-    # Register signal handlers for graceful exit
     signal.signal(signal.SIGTERM, handle_exit)
     signal.signal(signal.SIGINT, handle_exit)
 
-    # Terminate any existing processes from previous runs
-    terminate_existing_processes()
+    terminate_existing_processes()  # Terminate any existing processes from previous runs
 
-    # Read the ecosystem configuration file
     config = read_config(kwargs["config_file"])
     services = config["services"]
 
-    # Start all service instances
     total_cpus = os.cpu_count()
-
     services_started = False
 
     for service in services:
         if service.get("disabled", False):
             logger.info(f"Service '{service['name']}' is disabled. Skipping...")
-
             continue
 
         instances = service.get("instances", 1)
-
         if instances < 0:
             instances = max(
                 1, total_cpus - abs(instances)
@@ -324,24 +288,19 @@ def main(**kwargs):
 
         for i in range(instances):
             process = start_service(service, i)
-
             services_started = True
 
             if service.get("wait_ready", False):
                 thread = Thread(target=monitor_service, args=(service, process, i))
-
                 thread.start()
 
     if not services_started:
         logger.error("Error: No services to start. Exiting...")
-
         sys.exit(1)
 
-    # Wait for script termination
     try:
         while True:
             time.sleep(1)
-
     except KeyboardInterrupt:
         logger.info("Terminating services via keyboard exit...")
         cleanup_processes()
@@ -388,12 +347,13 @@ def stop_daemon():
             pid = int(f.read().strip())
             os.kill(pid, signal.SIGTERM)
             logger.info("Daemon stopped successfully.")
-            # Clear the PID file
-            os.remove(PID_FILE)
+            os.remove(PID_FILE)  # Clear the PID file
     except FileNotFoundError:
         logger.error("PID file not found. Is the daemon running?")
     except ProcessLookupError:
         logger.error("No such process. The daemon may have already stopped.")
+        if os.path.exists(PID_FILE):
+            os.remove(PID_FILE)  # Remove the stale PID file if it exists
     except Exception as e:
         logger.error(f"Error stopping daemon: {e}")
 
